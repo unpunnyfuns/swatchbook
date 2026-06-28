d3324d7a5a5f61ce6a1a12fd7edf85e377a17965
unpunnyfuns <unpunnyfuns@gmail.com>
2026-06-28 04:30:36 +0200

    docs(storybook): add a Tokens section introduction page

--- apps/storybook/.storybook/preview.tsx
+++ apps/storybook/.storybook/preview.tsx
@@ -38,6 +38,7 @@ export default definePreview({
           ],
           'Tokens',
           [
+            'Introduction',
             'Overview',
             'Colors',
             'Typography',
